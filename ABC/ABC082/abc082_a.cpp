73870a583bc1a224c5c636a5e8de08d2acd69dd5
gota_morishita <optimistic.gota@gmail.com>
2018-12-16 20:05:15 +0900

    solved ABC082 A

---
+++ ABC/ABC082/abc082_a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  int a, b;
+  std::cin >> a >> b;
+
+  if ((a + b) % 2 == 0) std::cout << (a + b) / 2 << std::endl;
+  else std::cout << ( a + b ) / 2 + 1 << std::endl;
+
+  return 0;
+}
