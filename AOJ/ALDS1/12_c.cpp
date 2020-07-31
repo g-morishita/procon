747a45ae85855da170cca63ef1fe69f83edf60f0
noteGmori <morishita.g@pieceofcake.co.jp>
2020-07-31 09:04:25 +0900

    solve a lot

---
+++ AOJ/ALDS1/12_c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+static const int MAX = 10000;
+static const int INFTY = (1 << 20);
+static const int WHITE = 0;
+static const int GRAY = 1;
+static const int BLACK = 2;
+int n;
+vector<pair<int, int>> adj[MAX];
+
+void dijkstra() {
+
+}
